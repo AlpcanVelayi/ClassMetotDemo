2cbb656ed8dd9ebdd0772d1c0e8b142d1bd3ee98
AlpcanVelayi <alpcanv@gmail.com>
2021-02-28 21:09:22 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine("Sepete Eklendi"+" "+ musteri.Id +" "+ musteri.Adi +" "+ musteri.Soyadi +" "+ musteri.Yasi +" "+ musteri.TcKimlikNo);
+        }
+        public void Listeleme(Musteri [] musteriler) 
+        {
+            Console.WriteLine("İstenen müşteriler aşağıda listelenmiştir :" );
+            foreach (Musteri musteri in musteriler )
+            {
+                Console.WriteLine($"Adı : " + musteri.Adi + "\n" + "Soyadı : " + musteri.Soyadi + "\n" + "Yaşı : " + musteri.Yasi + "\n" + "Tc Kimlik Numarası : " + musteri.TcKimlikNo);
+            }
+        }
+        public void Silme(Musteri musteri) 
+        {
+            Console.WriteLine("Seçtiğiniz müşteri silindi " + musteri.Id);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Alpcan";
+            musteri2.Soyadi = "Velayi";
+            musteri2.Yasi = 25;
+            musteri2.TcKimlikNo = 32869765400;
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 3;
+            musteri1.Adi = "Gözde";
+            musteri1.Soyadi = "Doğuer";
+            musteri1.Yasi = 25;
+            musteri1.TcKimlikNo = 40255072612;
+
+           
+
+            Musteri[] musteriler = { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            musteriManager.Listeleme(musteriler);
+           
+
+            musteriManager.Silme(musteri1);
+            musteriManager.Silme(musteri2);
+        }
+    }
+}
